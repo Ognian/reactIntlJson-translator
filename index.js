a3fa8751207fd7fccf13371ed05bbaee5ca9dce1
Ognian Tschakalov <ognian.tschakalov@ogi-it.com>
2016-06-01 15:21:38 +0200

    - initial spec

---
+++ index.js
@@ -0,0 +1,67 @@
+/**
+ * Created by ogi on 01.06.16.
+ */
+
+/**
+ * TODO
+ *
+ * Prepare a delta json file(s) for manual translation
+ * trans delta -c .trans.json
+ *      read the newly generated reactIntlJson file,
+ *      merge translations from the TM file(s) and
+ *      output json file(s) for manual translation, containing only the messages not contained in the TM.
+ *
+ * Prepare a full json file(s) for editing and/or manual translation
+ * trans all -c .trans.json
+ *      read the newly generated reactIntlJson file,
+ *      merge translations from the TM file(s) and
+ *      output json file(s) for manual translation, containing all messages translated and untranslated.
+ *
+ * Update the translation memory TM file(s) with the manual translated file(s) and generate the updated reactIntlJson file
+ * trans merge -c .trans.json
+ *
+ *
+ * config file .trans.json
+ *
+ *
+ * */
+
+var defaultConfig = {
+    defaultLanguage: "en",
+    inputJson: "dist/reactIntlMessages.json",
+
+    lanuages: ["de"],
+    toBeTranslatedJson: {
+        de: "transTmp/translate-de.json"
+    },
+    translationMemoryTMX: {
+        de: "src/intl/TM-de.tmx"
+    },
+    outputJson: {
+        de: "src/intl/reactIntlMessages-de.json"
+    }
+};
+
+
+var xml2js = require('xml2js');
+var fs = require('fs');
+
+var parser = new xml2js.Parser();
+fs.readFile('test/TM.tmx', function (err, data) {
+    parser.parseString(data, function (err, result) {
+        console.log("result:", JSON.stringify(result, undefined, 2));
+
+        // the JSON structure of such a file is:
+        //  .header
+        //  .body
+        //  body.tu is an array of the `translation unit`s
+        //  each `translation unit` contains one tuv `translation unit vector`?
+        //  each `translation unit vector` is an array of `segment`s
+        //  each `segment` has the language in .$.xml-lang and the string in .seg
+
+        // since we use ONE TM for every language our translation unit vectors contain only 2 elements the source language and the destination language
+        // so we could fill up our simpleTM and make it just an array of { en: en_seg, de: de_seg}
+
+
+    });
+});
